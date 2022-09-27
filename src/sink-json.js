0a5c7bae2651a2e8ca84f6f68f92dcb53afa501b
erxclau <ericlau41802@gmail.com>
2022-09-27 17:41:22 -0400

    Bump version

--- src/sink-json.js
+++ src/sink-json.js
@@ -28,7 +28,7 @@ const main = async (opts) => {
 const self = fileURLToPath(import.meta.url);
 if (process.argv[1] === self) {
   program
-    .version("2.0.0")
+    .version("2.0.1")
     .option("-c, --config <path>", "path to config file")
     .parse();
 
--- src/sink.js
+++ src/sink.js
@@ -3,7 +3,7 @@
 import { program } from "commander";
 
 program
-  .version("2.0.0")
+  .version("2.0.1")
   .name("sink")
   .description("Utility scripts")
   .command("gdoc", "fetch ArchieML Google Doc into JSON file")
