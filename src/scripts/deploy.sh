cb3c24af912b638f0844ed156cb62d0418a39eb8
Eric Lau <44459660+erxclau@users.noreply.github.com>
2022-10-13 20:23:15 -0400

    Use build variable over dist (#39)

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,14 +1,14 @@
 rm -rf $1
 git worktree add --detach $1
-(cd dist; git checkout --orphan gh-pages)
-(cd dist; git reset --hard)
-(cd dist; git pull -s ours --no-edit origin gh-pages --allow-unrelated-histories || echo "Could not pull from origin.")
+(cd $1; git checkout --orphan gh-pages)
+(cd $1; git reset --hard)
+(cd $1; git pull -s ours --no-edit origin gh-pages --allow-unrelated-histories || echo "Could not pull from origin.")
 
 yarn build
 
 BRANCH=$(git branch --show-current)
-(cd dist; git add --all)
-(cd dist; git commit -m "Build output as of $(git log '--format=format:%H' $BRANCH -1)" || echo "No changes to commit.")
-(cd dist; git push -u origin gh-pages)
+(cd $1; git add --all)
+(cd $1; git commit -m "Build output as of $(git log '--format=format:%H' $BRANCH -1)" || echo "No changes to commit.")
+(cd $1; git push -u origin gh-pages)
 git worktree remove $1
 git branch -D gh-pages
