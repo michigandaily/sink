154c73c7c5c67a6487abf7870ab517c76eeaa6cd
erxclau <ericlau41802@gmail.com>
2023-01-05 15:10:21 -0500

    Bump version

--- src/sink-deploy.js
+++ src/sink-deploy.js
@@ -240,7 +240,7 @@ const main = async ([platform], opts) => {
 
 if (process.argv[1] === self) {
   program
-    .version("2.5.1")
+    .version("2.6.0")
     .addArgument(
       new Argument("<platform>", "platform to deploy to").choices([
         "aws",
