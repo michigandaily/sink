899c4f06faec45fdf14b817f8fdee50e0c2b4df7
erxclau <ericlau41802@gmail.com>
2023-09-19 18:48:35 -0400

    Exit if deployment fields are missing
    Close #91

--- src/sink-deploy.js
+++ src/sink-deploy.js
@@ -83,7 +83,22 @@ const main = async ([platform], opts) => {
 
   if (platform === "aws") {
     const { region, bucket, key, build, profile } = config.deployment;
-    if (key.length < 1) {
+    if (region === null || region === undefined) {
+      console.error("no AWS region was specified. exiting.");
+      exit(1);
+    }
+
+    if (bucket === null || bucket === undefined) {
+      console.error("no AWS bucket was specified. exiting.");
+      exit(1);
+    }
+
+    if (build === null || build === undefined) {
+      console.error("no build directory was specified. exiting.");
+      exit(1);
+    }
+
+    if (key === undefined || key === null || key.length < 1) {
       const prompt = createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -324,6 +339,11 @@ const main = async ([platform], opts) => {
     }
   } else if (platform === "github") {
     const { build, branch, url } = config.deployment;
+
+    if (build === null || build === undefined) {
+      console.error("no build directory was specified. exiting.");
+      exit(1);
+    }
 
     const deploy = join(dirname(self), "scripts", "deploy.sh");
     execSync(
