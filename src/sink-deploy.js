1e2f6c87e31d88141e01eb9bb45726bf41f13fec
yumarie <yumarie@umich.edu>
2026-02-27 23:41:03 -0500

    bump version

--- src/sink-deploy.js
+++ src/sink-deploy.js
@@ -362,7 +362,7 @@ const main = async ([platform], opts) => {
 
 if (process.argv[1] === self) {
   program
-    .version("3.0.1")
+    .version("3.1.0")
     .addArgument(
       new Argument("<platform>", "platform to deploy to").choices(["aws"])
     )
--- src/sink.js
+++ src/sink.js
@@ -3,7 +3,7 @@
 import { program } from "commander";
 
 program
-  .version("3.0.1")
+  .version("3.1.0")
   .name("sink")
   .description("Utility scripts")
   .command("gdoc", "fetch ArchieML Google Doc into JSON file")
